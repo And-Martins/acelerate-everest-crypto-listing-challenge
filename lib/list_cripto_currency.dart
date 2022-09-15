17d2c74c8be2b515a3ec4b52681d2b355c15bc74
André <andre.martins@warren.com.br>
2022-09-15 17:39:36 -0300

    feature: turning graphic dinamic and bigger

--- lib/list_cripto_currency.dart
+++ lib/list_cripto_currency.dart
@@ -7,12 +7,16 @@ List<FlSpot> listCriptoCurrency() {
 
   var val = Random();
   for (var i = 0; i < 90; i++) {
-    yValues.add(val.nextDouble());
+    yValues.add(val.nextDouble() * 10);
   }
 
   List<FlSpot> spots = yValues.asMap().entries.map((e) {
     return FlSpot(e.key.toDouble(), e.value);
   }).toList();
+
+  for (var i = 0; i < 90; i++) {
+    print(spots[i]);
+  }
 
   return spots;
 }
