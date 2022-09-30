d70ad24454e81ea3ce5a68822ba5b72f9a4ec4c1
André <andre.martins@warren.com.br>
2022-09-30 15:05:22 -0300

    feat: adding helper message

--- lib/convert/widgets/convert_body.dart
+++ lib/convert/widgets/convert_body.dart
@@ -22,131 +22,150 @@ class ConvertBody extends StatefulHookConsumerWidget {
 class _ConvertBodyState extends ConsumerState<ConvertBody> {
   @override
   Widget build(BuildContext context) {
+    TextEditingController formFieldController = TextEditingController();
+    const limitReachedMessage = SnackBar(
+      backgroundColor: Colors.red,
+      content: Text('Quantidade inserida é maior que a quantidade disponível!'),
+    );
+
     final cryptoData = ref.watch(listCryptoProvider);
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          color: Colors.grey.shade200,
-          height: 50,
-          child: Padding(
-            padding: const EdgeInsets.all(15),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                const DefaultSubtitle('Saldo disponível'),
-                DefaultSubtitle(
-                  "${formatCriptoCompleto.format(DecimalIntl(Decimal.parse(ref.watch(criptoQtdWalletCriptoProvider).toString())))} ${ref.watch(fromCryptoConvertAbrev).toUpperCase()}",
-                  strong: 600,
-                ),
-              ],
-            ),
-          ),
-        ),
-        const Padding(
-          padding: EdgeInsets.all(20.0),
-          child: DefaultTitle(
-            title: "Quanto você gostaria de converter?",
-            strong: 700,
-            titleSize: 28,
-          ),
-        ),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            DropdownCrypto(
-                cryptoData: cryptoData,
-                type: "from",
-                fromCrypto: ref.watch(fromCryptoConvertAbrev)),
-            const Icon(
-              Icons.sync_alt_rounded,
-              color: Colors.red,
-            ),
-            DropdownCrypto(
-              cryptoData: cryptoData,
-              type: "to",
-            ),
-          ],
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20.0),
-          child: TextFormField(
-            style: GoogleFonts.montserrat(
-              fontSize: 35,
-              color: const Color.fromRGBO(149, 151, 166, 1),
-              fontWeight: FontWeight.w500,
-            ),
-            keyboardType: TextInputType.number,
-            cursorHeight: 35,
-            decoration: InputDecoration(
-              hintText:
-                  "${ref.watch(fromCryptoConvertAbrev).toUpperCase()} 0,00",
-              hintStyle: GoogleFonts.montserrat(
-                fontSize: 35,
-                color: const Color.fromRGBO(149, 151, 166, 1),
-                fontWeight: FontWeight.w500,
+    return SingleChildScrollView(
+      physics: const BouncingScrollPhysics(),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Container(
+            color: Colors.grey.shade200,
+            height: 50,
+            child: Padding(
+              padding: const EdgeInsets.all(15),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const DefaultSubtitle('Saldo disponível'),
+                  DefaultSubtitle(
+                    "${formatCriptoCompleto.format(DecimalIntl(Decimal.parse(ref.watch(criptoQtdWalletCriptoProvider).toString())))} ${ref.watch(fromCryptoConvertAbrev).toUpperCase()}",
+                    strong: 600,
+                  ),
+                ],
               ),
             ),
           ),
-        ),
-        const Padding(
-          padding: EdgeInsets.only(top: 8.0, right: 310),
-          child: DefaultSubtitle(
-            'R\$ 0,00',
-            strong: 500,
-          ),
-        ),
-       //TODO Substituir esse sizedBox por algo melhor 
-        const SizedBox(height: 200),
-        Column(
-          children: [
-            Divider(
-              color: Colors.grey.shade300,
-              thickness: 2.5,
+          const Padding(
+            padding: EdgeInsets.all(20.0),
+            child: DefaultTitle(
+              title: "Quanto você gostaria de converter?",
+              strong: 700,
+              titleSize: 28,
             ),
-            Row(
-              crossAxisAlignment: CrossAxisAlignment.end,
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Padding(
-                      padding: EdgeInsets.all(8.0),
-                      child: DefaultSubtitle(
-                        'Total estimado',
-                        strong: 500,
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 8.0),
-                      child: DefaultSubtitle(
-                        '0,0000 ${ref.watch(fromCryptoConvertAbrev).toUpperCase()}',
-                        strong: 600,
-                        color: Colors.black,
-                      ),
-                    ),
-                  ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              DropdownCrypto(
+                  cryptoData: cryptoData,
+                  type: "from",
+                  fromCrypto: ref.watch(fromCryptoConvertAbrev)),
+              const Icon(
+                Icons.sync_alt_rounded,
+                color: Colors.red,
+              ),
+              DropdownCrypto(
+                cryptoData: cryptoData,
+                type: "to",
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 15),
+            child: TextFormField(
+              onChanged: (value) {
+                
+              },
+              controller: formFieldController,
+              style: GoogleFonts.montserrat(
+                fontSize: 30,
+                color: const Color.fromRGBO(149, 151, 166, 1),
+                fontWeight: FontWeight.w500,
+              ),
+              keyboardType: TextInputType.number,
+              cursorHeight: 35,
+              decoration: InputDecoration(
+                hintText:
+                    "${ref.watch(fromCryptoConvertAbrev).toUpperCase()} 0,00",
+                hintStyle: GoogleFonts.montserrat(
+                  fontSize: 35,
+                  color: const Color.fromRGBO(149, 151, 166, 1),
+                  fontWeight: FontWeight.w500,
                 ),
-                IconButton(
-                  onPressed: () {},
-                  iconSize: 58,
-                  icon: CircleAvatar(
-                    backgroundColor: Colors.grey.shade400,
-                    radius: 40,
-                    child: const Icon(
-                      Icons.arrow_forward,
-                      size: 25,
-                      color: Colors.white,
+              ),
+            ),
+          ),
+          const Padding(
+            padding: EdgeInsets.only(top: 8.0, right: 310),
+            child: DefaultSubtitle(
+              'R\$ 0,00',
+              strong: 500,
+            ),
+          ),
+          //TODO Substituir esse sizedBox por algo melhor
+          const SizedBox(height: 180),
+          Column(
+            children: [
+              Divider(
+                color: Colors.grey.shade300,
+                thickness: 2.5,
+              ),
+              Row(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: DefaultSubtitle(
+                          'Total estimado',
+                          strong: 500,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                        child: DefaultSubtitle(
+                          '0,0000 ${ref.watch(fromCryptoConvertAbrev).toUpperCase()}',
+                          strong: 600,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ],
+                  ),
+                  IconButton(
+                    onPressed: () {
+                      if (ref.watch(criptoQtdWalletCriptoProvider) <
+                          double.parse(formFieldController.text)) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(limitReachedMessage);
+                      }
+                    },
+                    iconSize: 58,
+                    icon: CircleAvatar(
+                      backgroundColor: Colors.grey.shade400,
+                      radius: 40,
+                      child: const Icon(
+                        Icons.arrow_forward,
+                        size: 25,
+                        color: Colors.white,
+                      ),
                     ),
                   ),
-                ),
-              ],
-            ),
-          ],
-        )
-      ],
+                ],
+              ),
+            ],
+          )
+        ],
+      ),
     );
   }
 }
