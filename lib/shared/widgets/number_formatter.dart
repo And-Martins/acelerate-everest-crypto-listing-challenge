053a0da81a8c5b1c29cd7a3e043e9d1303aa1e9a
André <andre.martins@warren.com.br>
2022-10-06 12:10:17 -0300

    feat: changing visual of screen

--- lib/shared/widgets/number_formatter.dart
+++ lib/shared/widgets/number_formatter.dart
@@ -3,3 +3,4 @@ import 'package:intl/intl.dart';
 final formatReais = NumberFormat('#,###.##', 'pt_BR');
 final formatCriptoAbrev = NumberFormat('#.##', 'en_US');
 final formatCriptoCompleto = NumberFormat('#0.#########', 'en_US');
+final formatDate = DateFormat('dd/MM/yyyy');
--- lib/transactions/view/transactions_screen.dart
+++ lib/transactions/view/transactions_screen.dart
@@ -1,9 +1,11 @@
+import 'package:crypto_listing/shared/widgets/default_subtitle.dart';
 import 'package:crypto_listing/transactions/model/transaction_model.dart';
 import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../shared/widgets/default_navbar.dart';
 import '../../shared/widgets/default_title.dart';
+import '../../shared/widgets/number_formatter.dart';
 import '../providers/transaction_providers.dart';
 
 class TransactionsScreen extends StatefulHookConsumerWidget {
@@ -23,28 +25,80 @@ class _TransactionsScreenState extends ConsumerState<TransactionsScreen> {
   Widget build(BuildContext context) {
     List<TransactionModel> transaction = [];
     return Scaffold(
-      body: Column(
-        children: [
-          const Padding(
-            padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 55),
-            child: DefaultTitle(title: "Movimentações"),
-          ),
-          Expanded(
-            child: ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              itemCount: ref.watch(transactionsProvider).length,
-              itemBuilder: (BuildContext context, int index) {
-                transaction
-                    .add(ref.watch(transactionsProvider.state).state[index]);
-                return Column(
-                  children: [
-                    Text(transaction[index].fromCryptoAbrev.toString()),
-                  ],
-                );
-              },
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 25, horizontal: 10),
+              child: DefaultTitle(title: "Movimentações"),
             ),
-          ),
-        ],
+            Expanded(
+              child: ListView.builder(
+                physics: const BouncingScrollPhysics(),
+                itemCount: ref.watch(transactionsProvider).length,
+                itemBuilder: (BuildContext context, int index) {
+                  transaction
+                      .add(ref.watch(transactionsProvider.state).state[index]);
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 10),
+                    child: Column(
+                      children: [
+                        const Divider(
+                          thickness: 2,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  DefaultSubtitle(
+                                    "${formatCriptoCompleto.format(transaction[index].fromCryptoQtd)} ${transaction[index].fromCryptoAbrev.toUpperCase()}",
+                                    strong: 600,
+                                    size: 20,
+                                    color: Colors.grey.shade500,
+                                  ),
+                                  DefaultSubtitle(
+                                    formatDate
+                                        .format(transaction[index].date)
+                                        .toString(),
+                                    strong: 600,
+                                    size: 20,
+                                    color: Colors.grey.shade500,
+                                  ),
+                                ],
+                              ),
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  DefaultSubtitle(
+                                    "${formatCriptoCompleto.format(transaction[index].toCryptoQtd)} ${transaction[index].toCryptoAbrev.toUpperCase()}",
+                                    strong: 600,
+                                    size: 20,
+                                    color: Colors.black87,
+                                  ),
+                                  DefaultSubtitle(
+                                    "R\$ ${formatReais.format(transaction[index].valueReais)}",
+                                    strong: 600,
+                                    size: 20,
+                                    color: Colors.grey.shade500,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+              ),
+            ),
+          ],
+        ),
       ),
       bottomNavigationBar: const DefaultNavbar(
         selectedIndex: 1,
