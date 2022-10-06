bb3fb1eb65fa6860e5d60aa6532db0e461ebbefa
André <andre.martins@warren.com.br>
2022-10-06 09:01:24 -0300

    fix: applying sugestions

--- lib/convert/view/convert_screen.dart
+++ lib/convert/view/convert_screen.dart
@@ -1,18 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 import '../../shared/widgets/default_appbar.dart';
 import '../widgets/confirm_button.dart';
 import '../widgets/convert_body.dart';
 
-class ConvertScreen extends HookConsumerWidget {
+class ConvertScreen extends StatelessWidget {
   const ConvertScreen({Key? key}) : super(key: key);
   static const zeroAlert = SnackBar(
     backgroundColor: Colors.red,
     content: Text('Por favor insira um valor!'),
   );
   @override
-  Widget build(BuildContext context, WidgetRef ref) {
+  Widget build(BuildContext context) {
     return const Scaffold(
       appBar: DefaultAppbar(
         title: "Converter",
