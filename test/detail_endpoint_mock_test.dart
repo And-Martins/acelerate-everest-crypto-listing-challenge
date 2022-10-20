e7b2bb2214713228c6ad8a63ef4f24d5f463d9f9
André <andre.martins@warren.com.br>
2022-10-20 14:44:41 -0300

    feat: removing unnecessary code

--- test/detail_endpoint_mock_test.dart
+++ test/detail_endpoint_mock_test.dart
@@ -1,24 +1,17 @@
 import 'package:crypto_listing/cripto_details/endpoint/get_detail_endpoint.dart';
-import 'package:crypto_listing/cripto_details/model/detail_list_view_data.dart';
 import 'package:crypto_listing/cripto_details/repository/get_detail_repo.dart';
 import 'package:dio/dio.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import 'api_local/shared/api_factory.dart';
+import 'api_local/api_factory.dart';
 
 class DetailEndpointMock extends Mock implements GetDetailEndpoint {}
 
 void main() {
   late DetailEndpointMock detailEndpointMock;
   late GetDetailRepo getDetailRepo;
-
-  var prices = DetailListViewData(
-    prices: [
-      [faker.randomGenerator.decimal(scale: 10).toDouble()]
-    ],
-  );
 
   setUp(
     () {
--- test/wallet_endpoint_mock_test.dart
+++ test/wallet_endpoint_mock_test.dart
@@ -1,28 +1,17 @@
 import 'package:crypto_listing/wallet/endpoint/crypto_list_endpoint.dart';
-import 'package:crypto_listing/wallet/model/crypto_view_data.dart';
 import 'package:crypto_listing/wallet/repository/get_list/crypto_repo.dart';
 import 'package:dio/dio.dart';
 import 'package:faker/faker.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mocktail/mocktail.dart';
 
-import 'api_local/shared/api_factory.dart';
+import 'api_local/api_factory.dart';
 
 class WalletEndpointMock extends Mock implements CryptoEndpoint {}
 
 void main() {
   late WalletEndpointMock walletEndpointMock;
   late CryptoRepo cryptoRepo;
-
-  var cryptoViewData = CryptoViewData(
-    id: Faker().lorem.word(),
-    symbol: Faker().lorem.word(),
-    name: Faker().lorem.word(),
-    image: Faker().image.toString(),
-    currentPrice: Faker().randomGenerator.decimal(scale: 1000).toDouble(),
-    marketCapChangePercentage24h:
-        Faker().randomGenerator.decimal(scale: 5).toDouble(),
-  );
 
   setUp(
     () {
